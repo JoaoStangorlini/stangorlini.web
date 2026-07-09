89d2485a95eb6fa71d08f73f367ca96f79b22bfe
João Paulo <joaopaulostangorlini@usp.br>
2026-07-09 00:38:45 -0300

    refactor: replace Drive icon with SVG and update Google Drive URL

--- src/components/dashboard/QuickLinks.tsx
+++ src/components/dashboard/QuickLinks.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HubIcon = () => <img src="/icon.svg" alt="HUB" className="w-4 h-4 grayscale group-hover:grayscale-0 transition-all duration-300" />;
 const NotionIcon = () => <svg className="w-4 h-4 transition-colors" viewBox="0 0 24 24" fill="currentColor"><path d="M4.15 5.51L3 6.13v12.2l1.04.62 6.57 3.82 1.34.8 1.48-.82 7.57-4.4V5.5l-1.3-.7-6.52-3.8-1.5-.83-1.4.8zm13.78 11.83l-6.14 3.58-6.14-3.58V7.05l6.14-3.58 6.14 3.58v10.29zm-6.14-7.44v6.86l4.6-2.68v-4.18zM8.33 9.9v6.86l4.6 2.68V12.6z"/></svg>;
-const DriveIcon = () => <span className="material-symbols-outlined text-[16px] transition-colors">folder</span>;
+const DriveIcon = () => <svg className="w-4 h-4 transition-colors" viewBox="0 0 24 24" fill="currentColor"><path d="M9.17 6l2 2H20v10H4V6h5.17M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"/></svg>;
 const DiscordIcon = () => <svg className="w-4 h-4 transition-colors" viewBox="0 0 24 24" fill="currentColor"><path d="M20.317 4.37a19.791 19.791 0 00-4.885-1.515.074.074 0 00-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 00-5.487 0 12.64 12.64 0 00-.617-1.25.077.077 0 00-.079-.037A19.736 19.736 0 003.677 4.37a.07.07 0 00-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 00.031.057 19.9 19.9 0 005.993 3.03.078.078 0 00.084-.028 14.09 14.09 0 001.226-1.994.076.076 0 00-.041-.106 13.107 13.107 0 01-1.872-.892.077.077 0 01-.008-.128 10.2 10.2 0 00.372-.292.074.074 0 01.077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 01.078.01c.12.098.246.198.373.292a.077.077 0 01-.006.127 12.299 12.299 0 01-1.873.892.077.077 0 00-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 00.084.028 19.839 19.839 0 006.002-3.03.077.077 0 00.032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 00-.031-.028zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.956-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.946 2.418-2.157 2.418z"/></svg>;
 const ClarityIcon = () => <svg className="w-4 h-4 transition-colors" viewBox="0 0 24 24" fill="currentColor"><path d="M2.5 12a9.5 9.5 0 1119 0 9.5 9.5 0 01-19 0zm9.5-8a8 8 0 100 16 8 8 0 000-16zm-3 4v8h2v-8h-2zm4 3v5h2v-5h-2z"/></svg>;
 const SupabaseIcon = () => <svg className="w-4 h-4 transition-colors" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z"/></svg>;
@@ -22,7 +22,7 @@ export function QuickLinks() {
     { 
       name: 'Drive', 
       icon: <DriveIcon />, 
-      href: 'https://drive.google.com/drive/folders/1qLRWEIV2Wv2FpcLEFednjsN4BS9zSBQp?usp=sharing',
+      href: 'https://drive.google.com/drive/folders/1zRKy4EkOiqjS42G18RsJ4eIY8CN7SRNc?usp=drive_link',
       hoverClass: 'hover:border-[#FFD04B] hover:bg-gradient-to-r hover:from-[#4285F4]/20 hover:via-[#FFD04B]/20 hover:to-[#1FA463]/20 hover:text-[#4285F4]'
     },
     { 

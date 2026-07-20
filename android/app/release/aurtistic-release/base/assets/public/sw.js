bf1487221a86fc1c99bb32b8d508bacb8fec7c89
João Paulo <joaopaulostangorlini@usp.br>
2026-07-20 01:47:41 -0300

    Please provide the changes or file diffs you would like me to summarize.

---
+++ android/app/release/aurtistic-release/base/assets/public/sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => caches.delete(cacheName))
+      );
+    }).then(() => {
+      self.registration.unregister();
+    })
+  );
+});
